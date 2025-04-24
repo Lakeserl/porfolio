0404d8f392a1b7bd4412d8b0995de5db5b6cb7c8
Lakeserl <dinhlam18112003@gmail.com>
2025-04-24 23:24:49 +0700

    Updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faFacebook, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faMoon } from '@fortawesome/free-solid-svg-icons';
+import {FlexLayoutModule} from '@ngbracket/ngx-layout'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, FontAwesomeModule, FlexLayoutModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'Porfolio';
+  faFacebook = faFacebook;
+  faGithub = faGithub;
+  faMoon = faMoon;
 }
